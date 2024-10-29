a6624c3be305a1b1b4b2ed85b85142e8f2be4000
Ariel Alon <arielalon.12321@gmail.com>
2024-10-29 18:54:12 +0200

    Improve dB_meter graphics (less jumpy)

--- src/panel/dB_meter.c
+++ src/panel/dB_meter.c
@@ -14,6 +14,7 @@
 /* range used for searching the max value in the sample buffers */
 #define SEARCH_RANGE 1000
 
+/* TODO: Make this work in dB (currently just percentage) */
 void dB_meter_render(SampleBuffer *sample_buffer, Vector2 position, Vector2 size) {
     Texture2D dB_meter_texture = texture_load(TEXTURE_DB_METER);
     DrawTextureV(dB_meter_texture, position, WHITE);
@@ -25,18 +26,16 @@ void dB_meter_render(SampleBuffer *sample_buffer, Vector2 position, Vector2 size
     left  = sample_buffer_max_left(sample_buffer, SEARCH_RANGE);
     sample_buffer_unlock(sample_buffer);
 
-    // float right_dB = 20 * log10f(fmaxf(fabs(right), 1e-10));
-    // float left_dB  = 20 * log10f(fmaxf(fabs(left), 1e-10));
+    /* To prevent stutter every value 0.05f shy of full is converted into full height */
+    int max_height = size.y - 2;
+    int left_height =  MIN(left * max_height + 0.05f, max_height);
+    int right_height = MIN(right * max_height + 0.05f, max_height);
 
-    // /* TODO: Make this correct */
-    // float right_height = size.y * (right_dB - MIN_dB) / (MAX_dB - MIN_dB);
-    // float left_height  = size.y * (left_dB  - MIN_dB) / (MAX_dB - MIN_dB);
+    Vector2 left_pos = {position.x + 1, position.y + size.y - left_height - 1};
+    Vector2 right_pos = {position.x + size.x / 2, position.y + size.y - right_height - 1};
+    Vector2 left_dim = {size.x / 2 - 1, left_height};
+    Vector2 right_dim = {left_dim.x, right_height};
 
-    float left_height = left * size.y;
-    float right_height = right * size.y;
-    /* Draw left channel */
-    DrawRectangle(position.x + 1, position.y + size.y - left_height, size.x / 2 - 1, left_height, COLOR_WHITE);
-
-    /* Draw right channel */
-    DrawRectangle(position.x + size.x / 2, position.y + size.y - right_height, size.x / 2 - 1, right_height, COLOR_WHITE);
+    DrawRectangleV(left_pos, left_dim, COLOR_WHITE);
+    DrawRectangleV(right_pos, right_dim, COLOR_WHITE);
 }
